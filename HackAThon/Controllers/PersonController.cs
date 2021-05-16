dd1c5464cde7c9ad03cb8650b41d12041fae47b4
Mike Wong <mike.wong@create.nl>
2021-05-16 16:20:59 +0200

    make controller asynch

--- HackAThon/Controllers/PersonController.cs
+++ HackAThon/Controllers/PersonController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using HackAThon.DBContexts;
 using HackAThon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HackAThon
 {
@@ -18,39 +20,42 @@ namespace HackAThon
         }
 
         [HttpGet]
-        public List<Person> Get()
+        public async Task<List<Person>> Get()
         {
-            return _context.Person.ToList();
+            return await _context.Person.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public Person Get(int id)
+        public async Task<Person> Get(int id)
         {
-            return _context.Person.Single(Person => Person.Id == id);
+            return await _context.Person.SingleAsync(Person => Person.Id == id);
         }
 
         [HttpPost]
-        public void Post([FromBody] Person value)
+        public async Task<IActionResult> Post([FromBody] Person value)
         {
             var Person = new Person { Name = value.Name};
             _context.Person.Add(Person);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
+            return Ok(Person);
         }
 
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Person value)
+        public async Task<IActionResult> Put(int id, [FromBody] Person value)
         {
             var Person = _context.Person.Single(Person => Person.Id == id);
             Person.Name = value.Name;
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
+            return Ok(Person);
         }
 
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
             var Person = _context.Person.Single(Person => Person.Id == id);
             _context.Person.Remove(Person);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
+            return Ok(Person);
         }
     }
 }
